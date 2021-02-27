c6bc3347b3f466870951cec905aad0fdd002789b
Lorenz Gorse <logo@netlight.com>
2021-02-27 11:16:18 +0100

    Add some comments and do some clean ups.

--- src/jvmMain/kotlin/de/lorenzgorse/chatroulette/Chat.kt
+++ src/jvmMain/kotlin/de/lorenzgorse/chatroulette/Chat.kt
@@ -12,9 +12,16 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
+/**
+ * Every instance of this class handles one client that is connected to this server.
+ *
+ * @param clientIn the channel over which we receive WebSocket frames from the client.
+ * @param clientOut the channel over which we send WebSocket frames to the client.
+ * @param matchMaker the channel over which we communicate with the match maker.
+ */
 class Chat(
-        private val incoming: ReceiveChannel<Frame>,
-        private val outgoing: SendChannel<Frame>,
+        private val clientIn: ReceiveChannel<Frame>,
+        private val clientOut: SendChannel<Frame>,
         private val matchMaker: SendChannel<GetPeer>
 ) {
 
@@ -34,11 +41,17 @@ class Chat(
         activeUsers.incrementAndGet()
 
         try {
-            sendChatEvent(ChatEvent.UserId(user.id))
+            // We begin by informing the client of its user id.
+            sendToClient(ChatEvent.UserId(user.id))
+
             coroutineScope {
+                // This sends periodic status messages to the client.
                 val statusJob = launch { status() }
+
+                // This handles communication with peers (chat partners).
                 val peerJob = launch { while (loop()) { } }
 
+                // TODO: just cancel the whole scope.
                 select<Unit> {
                     statusJob.onJoin {
                         log.info("Status job exited, canceling peer job.")
@@ -56,11 +69,14 @@ class Chat(
         }
     }
 
+    /**
+     * Sends periodic status messages to the client.
+     */
     @ObsoleteCoroutinesApi
     private suspend fun status() {
         while (true) {
             try {
-                sendChatEvent(ChatEvent.UserCount(activeUsers.get()))
+                sendToClient(ChatEvent.UserCount(activeUsers.get()))
             } catch (e: ClosedSendChannelException) {
                 return
             }
@@ -68,12 +84,21 @@ class Chat(
         }
     }
 
+    /**
+     * This requests a new peer from the match maker and handles the communication with
+     * it once we get one. When this method returns, either the peer has disconnected
+     * (this is indicated by the return value `true`) or the user itself has
+     * disconnected (which is indicated by the return value `false`).
+     */
     @ExperimentalCoroutinesApi
     private suspend fun loop(): Boolean = coroutineScope {
+        // This consumes messages that the clients sends while we are not connected to
+        // a peer. It is cancelled once we get a peer (because then these messages
+        // should be forwarded to the peer).
         val consumeJob = launch {
             try {
                 while (true) {
-                    val msg = incoming.receive()
+                    val msg = clientIn.receive()
                     log.debug("Ignoring message $msg received from client while not connected to a peer.")
                 }
             } catch (_: ClosedReceiveChannelException) {
@@ -81,10 +106,13 @@ class Chat(
             }
         }
 
+        // Request a peer from the match maker.
         val getPeer = GetPeer()
         matchMaker.send(getPeer)
         log.info("Waiting for peer on channel ${getPeer.reply}.")
 
+        // Wait for the match maker to give us the channels that we can use to
+        // communicate with our peer.
         val channels =
             try {
                 select<Pair<ReceiveChannel<ChatEvent>, SendChannel<ChatEvent>>?> {
@@ -96,39 +124,51 @@ class Chat(
             }
 
         if (channels != null) {
-            val (inbox, outbox) = channels
+            val (peerIn, peerOut) = channels
             try {
+                // Cancel the consume job, because now we want to forward messages to
+                // our peer.
                 consumeJob.cancelAndJoin()
-                ConnectedState(inbox, outbox).loop()
+                // Enter the connected state.
+                ConnectedState(peerIn, peerOut).loop()
             } finally {
-                outbox.close()
+                peerOut.close()
             }
         } else false
     }
 
     inner class ConnectedState(
-            private val inbox: ReceiveChannel<ChatEvent>,
-            private val outbox: SendChannel<ChatEvent>
+            private val peerIn: ReceiveChannel<ChatEvent>,
+            private val peerOut: SendChannel<ChatEvent>
     ) {
 
         private val log = LoggerFactory.getLogger("user-${user.id}")!!
 
         /**
-         * Handles interaction with a connected peer. When this method returns, either the peer has disconnected (this is indicated by the return value `true`) or the user itself has disconnected (which is indicated by the return value `false`).
+         * Handles interaction with a connected peer. When this method returns, either
+         * the peer has disconnected (this is indicated by the return value `true`) or
+         * the user itself has disconnected (which is indicated by the return value
+         * `false`).
          */
         @ExperimentalCoroutinesApi
         suspend fun loop(): Boolean {
             log.info("New connection to peer.")
             try {
-                outbox.send(ChatEvent.Hello(user))
-                val hello = inbox.receive()
+                // Say hello to our peer.
+                peerOut.send(ChatEvent.Hello(user))
+
+                // Wait for our peer to say hello.
+                val hello = peerIn.receive()
                 require(hello is ChatEvent.Hello) {
                     "First message from peer must be hello, but was $hello." }
-                sendChatEvent(hello)
+
+                // Forward the peer's hello to the client.
+                sendToClient(hello)
+
                 while (true) {
                     select<Unit> {
-                        incoming.onReceive { handleFrame(it) }
-                        inbox.onReceive { handlePeerMessage(it) }
+                        clientIn.onReceive { handleClientMessage(it) }
+                        peerIn.onReceive { handlePeerMessage(it) }
                     }
                 }
             } catch (_: ClosedSendChannelException) {
@@ -138,35 +178,42 @@ class Chat(
             }
         }
 
-        private suspend fun handleFrame(frame: Frame) {
-            when (frame) {
-                is Frame.Text -> {
-                    val json = frame.readText()
-                    val chatEvent = Json.decodeFromString<ChatEvent>(json)
-                    outbox.send(chatEvent)
-                }
-                is Frame.Binary ->
-                    outbox.send(ChatEvent.Message.Image(frame.data))
-                else ->
+        private suspend fun handleClientMessage(frame: Frame) {
+            val chatEvent = when (frame) {
+                is Frame.Text -> Json.decodeFromString<ChatEvent>(frame.readText())
+                is Frame.Binary -> ChatEvent.Message.Image(frame.data)
+                else -> {
                     log.debug("Ignoring message: $frame")
+                    return
+                }
             }
+            peerOut.send(chatEvent)
         }
 
         private suspend fun handlePeerMessage(chatEvent: ChatEvent) {
-            sendChatEvent(chatEvent)
+            // TODO: we forward all chat events that we receive from our peer to out
+            //  client. This is not entirely correct. A peer could, for example, send a
+            //  ChatEvent.UserCount message, which changes the user count that is
+            //  displayed by our client. We should split the ChatEvent type into four
+            //  types:
+            //    - Events that we can send to our client
+            //    - Events that our client can send to us
+            //    - Events that we can send to our peer
+            //    - Events that our peer can send to us
+            sendToClient(chatEvent)
         }
 
         @ExperimentalCoroutinesApi
         private suspend fun handleClosedChannel(): Boolean =
                 when {
-                    outgoing.isClosedForSend || incoming.isClosedForReceive -> false
-                    outbox.isClosedForSend || inbox.isClosedForReceive -> peerDisconnected()
+                    clientOut.isClosedForSend || clientIn.isClosedForReceive -> false
+                    peerOut.isClosedForSend || peerIn.isClosedForReceive -> peerDisconnected()
                     else -> throw IllegalStateException()
                 }
 
         private suspend fun peerDisconnected(): Boolean =
                 try {
-                    sendChatEvent(ChatEvent.Disconnected)
+                    sendToClient(ChatEvent.Disconnected)
                     true
                 } catch (e: ClosedSendChannelException) {
                     false
@@ -174,17 +221,16 @@ class Chat(
 
     }
 
-    private suspend fun sendChatEvent(chatEvent: ChatEvent) {
-        when (chatEvent) {
+    private suspend fun sendToClient(chatEvent: ChatEvent) {
+        val frame = when (chatEvent) {
             is ChatEvent.Message.Image -> {
                 require(chatEvent.image is ByteArray)
-                outgoing.send(Frame.Binary(true, chatEvent.image))
+                Frame.Binary(true, chatEvent.image)
             }
-            else -> {
-                val json = Json.encodeToString(chatEvent)
-                outgoing.send(Frame.Text(json))
-            }
+            else ->
+                Frame.Text(Json.encodeToString(chatEvent))
         }
+        clientOut.send(frame)
     }
 
 }
